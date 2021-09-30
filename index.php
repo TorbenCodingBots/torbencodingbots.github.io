ce33b47a3a293e4cd36c0f17bfc1e24d731b20a9
TorbenCodingBots <91204421+TorbenCodingBots@users.noreply.github.com>
2021-09-30 08:00:16 -0400

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Contact Form - Torben Krahnstoever</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=devie-width", inital-sale=10">
-    <link re                                                                      
-  </head>
-<html>
